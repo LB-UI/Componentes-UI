34d330ff16a9e1b66e308bf5a2ac8f041e99947d
Miguel J Medina <pana9422@gmail.com>
2022-04-18 17:29:30 -0500

    Realizamos la configuraciÃn de webpack * Ignoramos los directorios /dist y /node_modules en el archivo .gitignore * Intalamos y configuramos webpack (webpack.config.js y app.js) * Agregamos utilitarios al body del index.html

---
+++ src/app.js
@@ -0,0 +1,1 @@
+import style from './app.css'
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path')
+const HTMLWebpackPLugin = require('html-webpack-plugin')
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin')
+
+
+module.exports = {
+    mode: 'development',
+    entry: './src/app.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'layblu.bundle.js',
+        assetModuleFilename: 'assets/[hash][ext]'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.html$/i,
+                loader: 'html-loader'
+            },
+            {
+                test: /\.css$/i,
+                use: [MiniCSSExtractPlugin.loader, 'css-loader']
+
+            },
+            {  
+                test: /\.(jpe?g|png|svg|webp|ico)$/i,
+                type: 'asset/resource'
+            },
+        ]
+    },
+    plugins: [
+        new HTMLWebpackPLugin({
+            template: 'src/index.html'
+        }),
+        new MiniCSSExtractPlugin({
+            filename: 'layblu.bundle.css'
+        })
+    ]
+
+}
